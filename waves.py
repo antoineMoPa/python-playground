6d54f02406a5843216b62f038dc5a2b496122aad
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2015-01-08 00:08:10 -0500

    transmission factor

--- waves.py
+++ waves.py
@@ -11,19 +11,28 @@ from copy import *
 class Waves:
     def __init__(self,w,h):
         self.w, self.h = w,h
-        self.data = np.ones( (self.w,self.h,4), dtype=np.uint8)
         self.time = 0
         self.interval = 0.2
-        self.heights = np.ones( (self.w,self.h), dtype=np.float32)
-        self.speeds = np.zeros( (self.w,self.h), dtype=np.float32)
         self.damping = 1.0
-        self.heights /= 2
-
         self.image = 0
+
+        self.clear()
 
         if not os.path.exists("./images"):
             os.makedirs("./images")
 
+    def clear(self):
+        # rgba data container
+        self.data = np.ones( (self.w,self.h,4), dtype=np.uint8)
+        # 'height' of every 'particle'
+        self.heights = np.ones( (self.w,self.h), dtype=np.float32 )
+        # their individual vertical speeds
+        self.speeds = np.zeros( (self.w,self.h), dtype=np.float32 )
+        # transmission factors
+        self.tfactors = np.ones( (self.w,self.h), dtype=np.float32 )
+        # make heights equal to 0.5
+        self.heights /= 2
+        
     def outputImg(self):
         img = self.getImage()
         img.save('./images/test'+str(self.image).zfill(6)+'.png')
@@ -33,21 +42,32 @@ class Waves:
         self.draw(channels)
         return Image.fromarray(self.data,'RGBA')
 
-    def point(self,j,i,radius,value):
+    def point(self,i,j,radius,value, arr="heights"):
         i = math.floor(i)
         j = math.floor(j)
+
+        if(arr == "heights"):
+            arr = self.heights
+        elif(arr == "speeds"):
+            arr = self.speeds
+        elif(arr == "tfactors"):
+            arr = self.tfactors
+        else:
+            print("error: arr must be either 'heights', 'speeds' or 'tfactors'")
+            return
+
         for k in range(i - radius,i + radius):
             for l in range(j - radius,j + radius):
                 d = self.dist(i,j,k,l)
                 if(d < radius):
-                    self.heights[k][l] += value * (1-d / radius)
+                    arr[k][l] += value * (1-d / radius)
 
     def dist(self,x1,y1,x2,y2):
         return math.sqrt(math.pow(x2 - x1,2) + math.pow(y2 - y1,2))
 
     def iterate(self):
         # we equilibrate with 4 cells so we divide effect of each cell by 4
-        factor = 1/8
+        factor = 1/4
         self.time += self.interval
         speeds = self.speeds
         heights = self.heights
@@ -66,10 +86,10 @@ class Waves:
         h3 = np.roll(np.copy(heights),1,axis=1)
         h4 = np.roll(np.copy(heights),-1,axis=1)
 
-        d1 = factor * (heights - h1)
-        d2 = factor * (heights - h2)
-        d3 = factor * (heights - h3)
-        d4 = factor * (heights - h4)
+        d1 = self.tfactors * factor * (heights - h1)
+        d2 = self.tfactors * factor * (heights - h2)
+        d3 = self.tfactors * factor * (heights - h3)
+        d4 = self.tfactors * factor * (heights - h4)
 
         speeds -= d1
         speeds -= d2
@@ -106,13 +126,15 @@ class Waves:
             try:
                 mult = float(channels[channel+"Multiplier"].get())
             except:
-                print("invalid multiplier")
+                print("invalid channel multiplier for "+channel)
                 mult = 1
 
             if (ch == "height"):
                 val = mult * self.heights
             elif (ch == "speed"):
                 val = mult * self.speeds + 0.5
+            elif (ch == "tfactor"):
+                val = self.tfactors
             elif (ch == "1"):
                 val = mult * 1
             elif (ch == "0"):
@@ -148,6 +170,7 @@ class Application(ttk.Frame):
         self.channelsOptions = [
             "height",
             "speed",
+            "tfactor",
             "1",
             "0"]
 
@@ -219,19 +242,32 @@ class Application(ttk.Frame):
         self.redraw_btn["text"] = "Redraw"
         self.redraw_btn["command"] = self.putWavesImage
 
+        self.clear_btn = ttk.Button(self.top_panel)
+        self.clear_btn["text"] = "Clear"
+        self.clear_btn["command"] = self.waves.clear
+
         self.play_btn.grid(column = 0, row=0, padx=5, pady=5)
 
         self.iterate_btn.grid(column = 1, row=0, padx=5, pady=5)
         self.redraw_btn.grid(column = 2, row=0, padx=5, pady=5)
+        self.clear_btn.grid(column = 3, row=0, padx=5, pady=5)
         self.top_panel.pack()
         self.middle_panel.pack()
 
-    def canvasDraw(self,event):
+    def canvasDraw(self,event,arr):
         # Safe zone
-        padding = 40
-        x = self.clipValue(event.x,padding,self.w - padding)
-        y = self.clipValue(event.y,padding,self.h - padding)
-        self.waves.point(x,y,20,0.1)
+        x = self.clipValue(event.x,0,self.w)
+        y = self.clipValue(event.y,0,self.h)
+
+        if(arr == "tfactors"):
+            size = 3
+            for i in range(y-size, y+size):
+                for j in range(x-size, x+size):
+                    self.waves.tfactors[i,j] = 0
+
+        else:
+            self.waves.point(y, x, 20, 0.1, arr=arr)
+
 
     def clipValue(self, val, min, max):
         if(val < min):
@@ -239,14 +275,25 @@ class Application(ttk.Frame):
         elif(val > max):
             val = max
         return val
-        
+
     def createCanvasWidgets(self):
         self.canvas = tk.Canvas(self.middle_panel,
                                 width = self.waves.w,
                                 height = self.waves.h)
-        
-        self.canvas.bind("<B1-Motion>",self.canvasDraw)
-        self.canvas.bind("<Button-1>",self.canvasDraw)
+
+        def canvasDrawHeights(event):
+            self.canvasDraw(event,"heights")
+
+        def canvasDrawTFactors(event):
+            self.canvasDraw(event,"tfactors")
+
+
+        self.canvas.bind("<B1-Motion>",canvasDrawHeights)
+        self.canvas.bind("<Button-1>",canvasDrawHeights)
+
+        self.canvas.bind("<B3-Motion>",canvasDrawTFactors)
+        self.canvas.bind("<Button-3>",canvasDrawTFactors)
+
         self.canvas.pack()
 
     def createColorSettingsWidgets(self):
