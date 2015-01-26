fd8e6ecb5a1ef74db536c452b5fa16e54d89ce2a
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2015-01-26 12:04:18 -0500

    some basic navigation

--- math/mandelbrot/mandelbrot.py
+++ math/mandelbrot/mandelbrot.py
@@ -16,64 +16,84 @@ class Simulation:
         self.time = 0
         self.interval = 0.2
         self.imagenum = 0
+                
+        self.clearC()
         self.clear()
-        # mouse
-        self.x = 0
-        self.y = 0
-
+        
         if not os.path.exists("./images"):
             os.makedirs("./images")
+    
+    def clearC(self):
+        # Initialize the values of c in z -> z**2 + c
+        
+        multR = 3
+        multI = 3
+        posR = 2
+        posI = 1.5
+        
+        self.c = 2
+        
+        self.cReal = np.zeros((self.w,self.h), dtype=np.float32)
+        self.cIm = np.zeros((self.w,self.h), dtype=np.float32)
+        for i in range(0,self.w):
+            for j in range(0,self.h):
+                self.cReal[i,j] = multR * j / (self.w) - posR
+                self.cIm[i,j] = multI * i / (self.w) - posI
 
     def clear(self):
         self.step = 0
         # rgba data container
         self.data = np.ones( (self.w,self.h,4), dtype=np.uint8)
 
-        self.cReal = np.zeros((self.w,self.h), dtype=np.float32)
-        self.cIm = np.zeros((self.w,self.h), dtype=np.float32)
         # real part
         self.real = np.zeros( (self.w,self.h), dtype=np.float32)
         # imaginary part
         self.im = np.zeros( (self.w,self.h), dtype=np.float32)
         # the set
         self.set = np.zeros( (self.w,self.h), dtype=np.int8)
-
-        self.multR = 3
-        self.multI = 3
-        self.posR = 2
-        self.posI = 1.5
-
-        for i in range(0,self.w):
-            for j in range(0,self.h):
-                self.cReal[i,j] = self.multR * j / (self.w) - self.posR
-                self.cIm[i,j] = self.multI * i / (self.w) - self.posI
+        
+        self.drawn = 0        
+        self.iteration = 0
+        self.iterations = 4
 
     def iterate(self):
-        self.step += 1
-        c = 2
-        self.real[np.abs(self.real) > c**2] = 0
-        self.im[np.abs(self.im) > c**2] = 0
-        # c = c**2 + c
-        a = self.real
-        b = self.im
-
-        # square complex number
-        aTemp = (a**2 - b**2)
-        b = b*a + a*b
-        a = aTemp
-        # addition
-        self.real = a + self.cReal
-        self.im = b + self.cIm
-        modulus = np.sqrt(self.real**2 + self.im**2)
-        self.set[(self.set == 0) & ((modulus) > c)] = 255-self.step
-        self.saveImage()
-
+        if(self.drawn == 1):
+            return
+        step = 0
+        limit = 2
+        for iteration in range(0,self.iterations):
+            step += 1
+            # protect against overflow
+            self.real[np.abs(self.real) > limit**2] = 0
+            self.im[np.abs(self.im) > limit**2] = 0
+            # c = c**2 + c
+            a = self.real
+            b = self.im
+            
+            # square complex number
+            aTemp = (a**2 - b**2)
+            b = b*a + a*b
+            a = aTemp
+            
+            aTemp = (a**2 - b**2)
+            b = b*a + a*b
+            a = aTemp
+            
+            
+            # addition
+            self.real = a + self.cReal
+            self.im = b + self.cIm
+            modulus = np.sqrt(self.real**2 + self.im**2)
+            self.set[(self.set == 0) & ((modulus) > limit)] = step
+            
+        #self.saveImage()
+        self.drawn = 1
 
     def createImage(self, canvas):
         canvas.delete("all")
-        self.data[:,:,0] = self.set
-        self.data[:,:,1] = self.set
-        self.data[:,:,2] = self.set
+        self.data[:,:,0] = self.set%10/10*255
+        self.data[:,:,1] = self.set%20/20*255
+        self.data[:,:,2] = self.set%50/50*255
         self.data[:,:,3] = 255
         self.image = Image.fromarray(self.data,'RGBA')
         self.tkimage = ImageTk.PhotoImage(self.image)
@@ -108,7 +128,7 @@ class Application(ttk.Frame):
 
     def iterate(self):
         res = 50
-
+        
         # time stats
         if(self.total_frames == res):
             # reset
@@ -116,6 +136,12 @@ class Application(ttk.Frame):
             self.total_time_between_frames = 0
 
         t = time.time()
+        
+        # pass settings
+        try:
+            self.simulation.iterations = int(self.iterations_num.get())
+        except:
+            pass
 
         # actual operation
         self.simulation.iterate()
@@ -158,22 +184,17 @@ class Application(ttk.Frame):
         self.play_btn["text"] = "Play"
         self.play_btn["command"] = self.play
 
-        self.iterate_btn = ttk.Button(self.top_panel)
-        self.iterate_btn["text"] = "Iterate"
-        self.iterate_btn["command"] = self.iterate
-
         self.redraw_btn = ttk.Button(self.top_panel)
         self.redraw_btn["text"] = "Redraw"
-        self.redraw_btn["command"] = self.putSimulationImage
+        self.redraw_btn["command"] = self.simulation.clear
 
         self.clear_btn = ttk.Button(self.top_panel)
         self.clear_btn["text"] = "Clear"
         self.clear_btn["command"] = self.simulation.clear
 
-        self.play_btn.grid(column = 0, row=0, padx=5, pady=5)
+        self.play_btn.grid(column = 0, row=0, padx=5, pady=5)        
         self.redraw_btn.grid(column = 1, row=0, padx=5, pady=5)
-        self.iterate_btn.grid(column = 2, row=0, padx=5, pady=5)
-        self.clear_btn.grid(column = 3, row=0, padx=5, pady=5)
+        self.clear_btn.grid(column = 2, row=0, padx=5, pady=5)
         self.top_panel.pack()
         self.middle_panel.pack()
 
@@ -204,36 +225,57 @@ class Application(ttk.Frame):
         self.canvas = tk.Canvas(self.middle_panel,
                                 width = self.simulation.w,
                                 height = self.simulation.h)
+        
+        self.zoom = 10
+        
+        def left(e=None):
+            self.simulation.cReal -= 1/self.zoom
+            self.simulation.clear() 
+        def right(e=None):
+            self.simulation.cReal += 1/self.zoom
+            self.simulation.clear()
+        def top(e):
+            self.simulation.cIm -= 1/self.zoom
+            self.simulation.clear()
+        def bottom(e):
+            self.simulation.cIm += 1/self.zoom
+            self.simulation.clear()
+        def zoomIn(e):
+            self.simulation.cReal *= 0.9
+            self.simulation.cIm *= 0.9
+            self.simulation.clear()
+        def zoomOut(e):
+            self.simulation.cReal *= 1.1
+            self.simulation.cIm *= 1.1
+            self.simulation.clear()
 
-        def updatePosition(event):
-            self.simulation.x = event.x
-            self.simulation.y = event.y
-
-        def click(e):
-            ps = self.simulation.particles
-            ps = np.append(ps, [[e.x, e.y, 0, 0]], axis=0)
-            self.simulation.particles= ps
-
-        #self.canvas.bind("<Motion>", motion)
-        self.canvas.bind("<Button-1>", click)
-
+        self.master.bind("<Left>", left)
+        self.master.bind("<Right>", right)
+        self.master.bind("<Up>", zoomIn)
+        self.master.bind("<Down>", zoomOut)
+        self.master.bind("<w>", top)
+        self.master.bind("<s>", bottom)
+        self.master.bind("<a>", left)
+        self.master.bind("<d>", right)
+        
+        
         self.canvas.pack()
 
     def createSettingsWidgets(self):
         # you can create settings and use them in your simulation
         self.settingsFrame = tk.Frame()
-        self.somesetting = tk.StringVar()
+        self.iterations_num = tk.StringVar()
 
-        self.somesetting.set(3)
+        self.iterations_num.set(10)
 
-        someSettingLabel = ttk.Label(self.settingsFrame,
-                                     text="Some setting")
-        someSetting = ttk.Entry(self.settingsFrame,
-                                textvariable=self.somesetting,
+        iterations_numLabel = ttk.Label(self.settingsFrame,
+                                     text="max iterations")
+        iterations_num = ttk.Entry(self.settingsFrame,
+                                textvariable=self.iterations_num,
                                 width=5, justify="r")
 
-        someSettingLabel.grid(row=0, column=0)
-        someSetting.grid(row=1, column=0)
+        iterations_numLabel.grid(row=0, column=0)
+        iterations_num.grid(row=1, column=0)
 
         self.settingsFrame.pack(pady=10)
 
