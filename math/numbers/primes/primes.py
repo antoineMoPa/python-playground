eb8bf865fc4ca48a80cb530ab4c71e5707d14857
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2015-03-17 15:47:15 -0400

    dumb probable prime & test

--- math/numbers/primes/primes.py
+++ math/numbers/primes/primes.py
@@ -15,7 +15,7 @@ def find_first_primes(num):
 def less_dumb_is_prime(num,first_primes):
     if len(first_primes) == 0:
         return dumb_is_prime(num)
-    
+
     for i in range(0,len(first_primes)):
         if num % first_primes[i] == 0:
             return False
@@ -31,12 +31,12 @@ def dumb_is_prime(num):
         return True
 
     a = 1
-    
+
     while a <= math.sqrt(num):
         a += 1
         if(num % a == 0):
             return False
-        
+
     return True
 
 def run_tests():
@@ -44,7 +44,7 @@ def run_tests():
     assert(dumb_is_prime(2))
     assert(dumb_is_prime(3))
     assert(find_first_primes(1000)[999] == 7919)
-    
+
     ten_first = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
     assert(find_first_primes(10) == ten_first)
 
@@ -61,9 +61,7 @@ def diff_info(num):
     print("diffs: "+str(diffs))
     print("max: "+str(max(diffs)))
 
-def dumb_is_probable_prime(num):
-    firsts = find_first_primes(10)
-    print("last first prime: "+str(firsts[-1]))
+def dumb_is_probable_prime(num, firsts):
     for i in range(0,len(firsts)):
         if firsts[i] > math.sqrt(num):
             return True
@@ -71,13 +69,39 @@ def dumb_is_probable_prime(num):
             return False
 
     return True
-    
+
 def test_probable_prime_func(func):
+    num_first = 10
+    firsts = find_first_primes(num_first)
+    print("last first prime: " + str(firsts[-1]))
+    positives = 0
+    false_positives = 0
+    false_negatives = 0
 
-    num = 663
-    print("validation:       "+str(dumb_is_prime(num)))
-    print("probability test: "+str(func(num)))
+    for  i in range(0,10000):
+        num = i
+        test = dumb_is_prime(num)
+        prob = func(num, firsts)
 
+        if(test and prob):
+            positives += 1
+        if(prob and not test):
+            false_positives += 1
+        if(not prob and test):
+            false_negatives += 1
+
+    # there should be no false negatives
+    assert(false_negatives == 0)
+
+    error_ratio = false_positives / positives
+    correct_ratio = 1 - error_ratio
+    percent_correct = correct_ratio*100
+    percent_correct_str = "%.2f" % (percent_correct)
+    
+    print("Primes found :       " + str(positives))
+    print("False primes found : " + str(false_positives))
+    print("Percentage correct:  " + percent_correct_str)
+    
 run_tests()
 #diff_info(10000)
 
