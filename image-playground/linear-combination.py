c7e55db535b1ef7307f7c01ebe9eb0c89a707a43
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2016-01-29 17:55:02 -0500

    this is weird enough now

--- image-playground/linear-combination.py
+++ image-playground/linear-combination.py
@@ -19,16 +19,15 @@ img = 1/3*(imgr + imgg + imgb)
 
 img /= np.max(img)
 
-grid_size = 200
-    
 def show(image):
     plt.imshow(image,cmap=cm.Greys_r)
     plt.show()
 
+grid_size = 80
+    
 part = np.empty(img.shape)
 
 max_steps = 2
-
 coefficients = np.empty((grid_size,grid_size,max_steps))
 
 cell_width = img.shape[1] / grid_size
@@ -42,7 +41,7 @@ for a in range(0,grid_size):
         ]
         part = np.empty(sub_img.shape)
         for step in range(1,max_steps,1):
-            k = step
+            k = step ** 2
             w = sub_img.shape[1]/k
             h = sub_img.shape[0]/k
             for j in range(0, k):
@@ -51,8 +50,7 @@ for a in range(0,grid_size):
                         part[j * h:(j+1)*h,i * w:(i+1)*w] = 1
                     else:
                         part[j * h:(j+1)*h,i * w:(i+1)*w] = -1
-                        
-            coefficients[a,b,step] = np.sum(part * sub_img)
+                    coefficients[a,b,step] = np.sum(part * sub_img)
                         
 
 final = np.zeros(img.shape)
@@ -67,7 +65,7 @@ for a in range(0,grid_size):
             ]
         part = np.empty(sub_img.shape)
         for step in range(1,max_steps,1):
-            k = step
+            k = step ** 30
             w = sub_img.shape[1]/k
             h = sub_img.shape[0]/k
             for j in range(0, k):
